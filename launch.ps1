7cfd6f293f5f82dbfe27363f9d25b0960db80700
DomainTyler <DomainTyler@outlook.com>
2025-07-03 02:07:03 +0100

    launch.ps1

--- launch.ps1
+++ launch.ps1
@@ -1,5 +1,5 @@
-$exeUrl = "https://raw.githubusercontent.com/DomainTyler/Quick-Tools-Launcher-by-DomainTyler/main/Quick%20Tools%20Launcher%20by%20DomainTyler.exe"
-$localPath = "$env:TEMP\QuickToolsLauncher.exe"
+$exeUrl = "https://raw.githubusercontent.com/DomainTyler/Quick-Tools-Launcher-by-DomainTyler/main/Quick%20Tools%20Launcher%20by%20DomainTyler%20v2.exe"
+$localPath = "$env:TEMP\QuickToolsLauncherV2.exe"
 
 Invoke-WebRequest -Uri $exeUrl -OutFile $localPath -UseBasicParsing
 
