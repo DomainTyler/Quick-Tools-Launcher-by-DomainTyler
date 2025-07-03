b29bc465c36721bcef0ac8be51ec36e941750d68
DomainTyler <DomainTyler@outlook.com>
2025-07-03 03:33:03 +0100

    Delete launch v2.ps1

--- launch v2.ps1
+++
@@ -1,6 +0,0 @@
-$exeUrl = "https://github.com/DomainTyler/Quick-Tools-Launcher-by-DomainTyler/raw/main/Quick%20Tools%20Launcher%20by%20DomainTyler%20v2.exe"
-$localPath = "$env:TEMP\QuickToolsLauncherV2.exe"
-
-Invoke-WebRequest -Uri $exeUrl -OutFile $outputPath -UseBasicParsing
-
-Start-Process -FilePath $localPath
